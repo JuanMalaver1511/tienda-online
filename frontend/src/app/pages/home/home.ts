9844a84fb423e9738b837a3d1a8e599b06bca71e
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2026-01-31 23:12:35 -0500

    new components

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { Portada } from '../../components/portada/portada';
+import { Products } from '../../components/products/products';
+import { Categorias } from "../../components/categorias/categorias";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Portada],
+  imports: [Navbar, Portada, Products, Categorias],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
